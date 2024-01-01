c286e7e75fa0774780b951639720de218aba7449
Your Name <you@example.com>
2024-01-01 21:30:59 +0800

    添加redis

--- src/main/java/com/xie/MainApplication.java
+++ src/main/java/com/xie/MainApplication.java
@@ -1,5 +1,6 @@
 package com.xie;
 
+import com.xie.config.RedisUtil;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,11 +16,16 @@ public class MainApplication extends WebMvcConfigurerAdapter implements CommandL
 
     @Autowired
     private DataSource dataSource;
+    @Autowired
+    private RedisUtil redisUtil;
+
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
+
     @Override
     public void run(String... args) throws Exception {
+        redisUtil.set("key1", "xxx");
         System.out.println();
     }
 }
