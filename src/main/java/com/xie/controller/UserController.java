9f6a25a5daad283e9e7861203a412d2cc97fe1c7
xiewz <xiewz@gempharmatech.com>
2024-01-02 12:09:49 +0800

    添加redis笔记

--- src/main/java/com/xie/controller/UserController.java
+++ src/main/java/com/xie/controller/UserController.java
@@ -3,6 +3,7 @@ package com.xie.controller;
 import com.xie.bean.User;
 import com.xie.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class UserController {
 
 
     @RequestMapping("/add")
+    @Transactional
     public String addUser(@PathVariable String name, @PathVariable String pwd) {
         return userService.addUser(name, pwd) ? "success" : "fail";
     }
