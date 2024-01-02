13430cf51dcd0f6151f6208494b1c05718956031
Your Name <you@example.com>
2024-01-02 11:06:17 +0800

    添加mvc课堂笔记

--- src/main/java/com/xie/MainApplication.java
+++ src/main/java/com/xie/MainApplication.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.sql.DataSource;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @MapperScan(basePackages = "com.xie.mapper")
 public class MainApplication extends WebMvcConfigurerAdapter implements CommandLineRunner {
 
--- src/main/java/com/xie/service/UserService.java
+++ src/main/java/com/xie/service/UserService.java
@@ -4,12 +4,14 @@ import com.xie.bean.User;
 import com.xie.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Transactional
     public boolean addUser(String userName, String pwd) {
         return userMapper.insert(userName, pwd) > 0 ? true : false;
     }
