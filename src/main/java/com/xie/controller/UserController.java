fface913b8e2fcf3c7400b11e2ca1f04569bc58e
xiewz <xiewz@gempharmatech.com>
2024-01-04 14:22:41 +0800

    添加@ResponseBody的解析和视图解析笔记

--- src/main/java/com/xie/controller/UserController.java
+++ src/main/java/com/xie/controller/UserController.java
@@ -5,10 +5,11 @@ import com.xie.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
 
@@ -16,30 +17,35 @@ public class UserController {
     private UserService userService;
 
 
-    @PostMapping(path = "/add", consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE,headers = "Content-Length1")
+    @PostMapping(path = "/add", consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public User addUser(@RequestBody User user) {
         String str = userService.addUser(user.getName(), user.getPwd()) ? "success" : "fail";
         return new User("aaa","bbb");
     }
-
-
-    @RequestMapping(path = "/add1/{name}/{pwd}")
-    @Transactional
-    public String addUser1(@PathVariable String name, @PathVariable String pwd) {
-        return userService.addUser(name, pwd) ? "success" : "fail";
+    @PostMapping(path = "/add1", consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public User addUser1(@RequestBody User user) {
+        String str = userService.addUser(user.getName(), user.getPwd()) ? "success" : "fail";
+        return new User("aaa","bbb");
     }
-
-    @RequestMapping(path = "/add2")
-    @Transactional
-    public String addUser2(@RequestParam String name, @RequestParam String pwd) {
-        return userService.addUser(name, pwd) ? "success" : "fail";
-    }
-
-
-    @RequestMapping("/get")
-    public User getUserByName(String userName) {
-        return userService.getUserByName(userName);
-    }
+//
+//
+//    @RequestMapping(path = "/add1/{name}/{pwd}")
+//    @Transactional
+//    public String addUser1(@PathVariable String name, @PathVariable String pwd) {
+//        return userService.addUser(name, pwd) ? "success" : "fail";
+//    }
+//
+//    @RequestMapping(path = "/add2")
+//    @Transactional
+//    public String addUser2(@RequestParam String name, @RequestParam String pwd) {
+//        return userService.addUser(name, pwd) ? "success" : "fail";
+//    }
+//
+//
+//    @RequestMapping("/get")
+//    public User getUserByName(String userName) {
+//        return userService.getUserByName(userName);
+//    }
 
 }
