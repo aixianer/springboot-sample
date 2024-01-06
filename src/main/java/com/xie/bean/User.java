e4b39f14dcd6f406754b6f1f20e37030ea9973e9
Your Name <you@example.com>
2024-01-06 16:37:50 +0800

    添加异常处理和spring mvc切入点笔记

--- src/main/java/com/xie/bean/User.java
+++ src/main/java/com/xie/bean/User.java
@@ -2,9 +2,12 @@ package com.xie.bean;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class User {
     private long id;
+    @NotNull(message = "名字不能为空")
     private String name;
     private String pwd;
 
